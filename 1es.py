726c1e36d156059b70280b17b850fccc3a7b384f
bbooss97 <bbooss97@gmail.com>
2021-11-19 17:19:29 +0100

    modifica con decimali

--- 1es.py
+++ 1es.py
@@ -4,13 +4,13 @@ gruppo=[[15.2,9.4,11.1],
         [4.9,2,3.5,2.9],
         [1,6.4,4.6,2.1]]
 d={}
-step=10
+decimali=1
 def ric(indicegruppo,massimo):
     if indicegruppo>=len(gruppo):
             return 0
     if indicegruppo==0:
         massimoLocale=0
-        for i in range(0,massimo):
+        for i in range(0,massimo+1):
             if (indicegruppo,i)in d:
                 return d[(indicegruppo,i)]
             regaliTotaliParenti=0
@@ -24,7 +24,7 @@ def ric(indicegruppo,massimo):
         return massimoLocale
 
     massimoLocale=0
-    for i in range(0,massimo):
+    for i in range(0,massimo+1):
         if (indicegruppo,i)in d :
             return d[(indicegruppo,i)]
         regaliTotaliParenti=0
@@ -39,9 +39,12 @@ def ric(indicegruppo,massimo):
 
 
 if __name__ == "__main__":
+    for i in range(len(gruppo)):
+        for j in range(len(gruppo[i])):
+            gruppo[i][j]*=(10*decimali)
     massimo=0
     for i in range(len(gruppo)):
         for j in range(len(gruppo[i])):
             if gruppo[i][j]>massimo:
                 massimo=gruppo[i][j]
-    print(ric(0,massimo))
+    risultato=ric(0,massimo)/(10*decimali)
