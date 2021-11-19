90c9974fc3675698510a7e056a0cb753c8c8742a
bbooss97 <bbooss97@gmail.com>
2021-11-19 19:47:24 +0100

    final

--- 1es.py
+++ 1es.py
@@ -1,44 +1,49 @@
-gruppo=[[15.2,9.4,11.1],
-        [9.7,12.2,7.5,10.8],  
-        [5.2,6.3,5.6,10.2],   
-        [4.9,2,3.5,2.9],
-        [1,6.4,4.6,2.1]]
+# gruppo=[[15.2,9.4,11.1],
+#         [9.7,12.2,7.5,10.8],  
+#         [5.2,6.3,5.6,10.2],   
+#         [4.9,2,3.5,2.9],
+#         [1,6.4,4.6,2.1]]
+
+
+gruppo=[
+    [9],
+    [9],
+    [9],
+    [9],
+    [9],
+    [9],
+    [9],
+    [9],
+    [9],
+    [9],
+    [9],
+    [9],
+    [9],
+    [9]
+]
+
+
+
 d={}
 decimali=1
 
 def ric(indicegruppo,massimo):
     if indicegruppo>=len(gruppo):
             return 0
-    if indicegruppo==0:
-        massimoLocale=0
-        for i in range(massimo+1):
-            #print(indicegruppo,i)
-            if (indicegruppo,i)in d:
-                if d[(indicegruppo,i)]>massimoLocale:
-                    massimoLocale=d[(indicegruppo,i)]
-                continue
-            regaliTotaliParenti=0
-            for j in range(len(gruppo[indicegruppo])):
-                if i<=gruppo[indicegruppo][j]:
-                    regaliTotaliParenti+=gruppo[indicegruppo][j]
-            val=regaliTotaliParenti+ric(indicegruppo+1,i)
-            d[(indicegruppo,i)]=val
-            if val>massimoLocale:
-                massimoLocale=val
-        return massimoLocale
-    
     massimoLocale=0
     for i in range(massimo+1):
-        if (indicegruppo,i)in d :
-            if d[(indicegruppo,i)]>massimoLocale:
-                massimoLocale=d[(indicegruppo,i)]
-            continue
         regaliTotaliParenti=0
+        if (indicegruppo,i,massimo)in d:
+            if d[(indicegruppo,i,massimo)]>massimoLocale:
+                massimoLocale=d[(indicegruppo,i,massimo)]
+            continue
         for j in range(len(gruppo[indicegruppo])):
-            if i<=gruppo[indicegruppo][j]:
-                regaliTotaliParenti+=i
+            if gruppo[indicegruppo][j]>=massimo:
+                regaliTotaliParenti+=massimo
+            elif i<=gruppo[indicegruppo][j]:
+                regaliTotaliParenti+=gruppo[indicegruppo][j]
         val=regaliTotaliParenti+ric(indicegruppo+1,i)
-        d[(indicegruppo,i)]=val
+        d[(indicegruppo,i,massimo)]=val
         if val>massimoLocale:
             massimoLocale=val
     return massimoLocale
@@ -53,9 +58,8 @@ if __name__ == "__main__":
         for j in range(len(gruppo[i])):
             if gruppo[i][j]>massimo:
                 massimo=gruppo[i][j]
-    
-    
     risultato=ric(0,massimo)/(10*decimali)
     #print(d)
+    
     print(risultato)
  
