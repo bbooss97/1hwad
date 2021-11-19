224ef8e37fbb09815eb124d7714dbc8454f6b791
bbooss97 <bbooss97@gmail.com>
2021-11-19 17:48:17 +0100

    correzioni

--- 1es.py
+++ 1es.py
@@ -5,6 +5,7 @@ gruppo=[[15.2,9.4,11.1],
         [1,6.4,4.6,2.1]]
 d={}
 decimali=1
+
 def ric(indicegruppo,massimo):
     if indicegruppo>=len(gruppo):
             return 0
@@ -14,7 +15,7 @@ def ric(indicegruppo,massimo):
             if (indicegruppo,i)in d:
                 return d[(indicegruppo,i)]
             regaliTotaliParenti=0
-            for j in len(gruppo[indicegruppo]):
+            for j in range(len(gruppo[indicegruppo])):
                 if i<=gruppo[indicegruppo][j]:
                     regaliTotaliParenti+=gruppo[indicegruppo][j]
             val=regaliTotaliParenti+ric(indicegruppo+1,i)
@@ -22,16 +23,17 @@ def ric(indicegruppo,massimo):
             if val>massimoLocale:
                 massimoLocale=val
         return massimoLocale
-
+        
     massimoLocale=0
     for i in range(0,massimo+1):
         if (indicegruppo,i)in d :
             return d[(indicegruppo,i)]
         regaliTotaliParenti=0
-        for j in len(gruppo[indicegruppo]):
+        for j in range(len(gruppo[indicegruppo])):
             if i<=gruppo[indicegruppo][j]:
                 regaliTotaliParenti+=i
         val=regaliTotaliParenti+ric(indicegruppo+1,i)
+        
         d[(indicegruppo,i)]=val
         if val>massimoLocale:
             massimoLocale=val
@@ -41,10 +43,13 @@ def ric(indicegruppo,massimo):
 if __name__ == "__main__":
     for i in range(len(gruppo)):
         for j in range(len(gruppo[i])):
-            gruppo[i][j]*=(10*decimali)
+            gruppo[i][j]=int(gruppo[i][j]*(10*decimali))
     massimo=0
     for i in range(len(gruppo)):
         for j in range(len(gruppo[i])):
             if gruppo[i][j]>massimo:
                 massimo=gruppo[i][j]
+
     risultato=ric(0,massimo)/(10*decimali)
+    print(risultato)
+  
