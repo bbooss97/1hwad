53777da37fe7fcced893e18c854fdf886ebecd53
bbooss97 <bbooss97@gmail.com>
2021-12-04 00:00:02 +0100

    rewq

--- 1es.py
+++ 1es.py
@@ -2,10 +2,11 @@ relatives=[[15.2,9.4,11.1],
         [9.7,12.2,7.5,10.8],  
         [5.2,6.3,5.6,10.2],   
         [4.9,2,3.5,2.9],
-        [1,6.4,4.6,2.1]]
+        [1,6.4,4.6,321]]
 numberOfDecimals=1
 dp={}
 def recursive(dist,pr):
+    dp={}
     if dist>=len(relatives):
             return 0
     maximumGift=0
@@ -24,6 +25,29 @@ def recursive(dist,pr):
     dp[(dist,pr)]=maximumGift
     return maximumGift
 
+def ric(dist,pr):
+    dp={}
+    if dist>=len(relatives):
+        return 0
+    maximumGift=0
+    if (dist,pr)in dp:
+            return dp[(dist,pr)]
+    lista=relatives[dist].copy()
+    lista.append(pr)
+    for i in lista:
+        totalValueAtRow=0
+        if i<=pr:
+            for j in range(len(relatives[dist])):
+                if relatives[dist][j]>=pr:
+                    totalValueAtRow+=pr
+                elif i<=relatives[dist][j]:
+                    totalValueAtRow+=relatives[dist][j]
+            val=totalValueAtRow+ric(dist+1,i)
+            if val>maximumGift:
+                maximumGift=val
+    dp[(dist,pr)]=maximumGift
+    return maximumGift
+
 if __name__ == "__main__":
     #vr=vr*10*approximation
     for i in range(len(relatives)):
@@ -34,6 +58,9 @@ if __name__ == "__main__":
         for j in range(len(relatives[i])):
             if relatives[i][j]>maximumVr:
                 maximumVr=relatives[i][j]
+
+    print(ric(0,maximumVr)/(10*numberOfDecimals))
     result=recursive(0,maximumVr)/(10*numberOfDecimals)
     print(result)#out=116.3
+    
  
